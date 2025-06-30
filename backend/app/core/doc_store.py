d6c5c8ab25b24f3493c947ef097dd303220b48f3
Azim Baghadiya <azimsb120@gmail.com>
2025-06-29 20:38:21 -0400

    chore: rename doc_store.py to document_store.py

--- backend/app/core/doc_store.py
+++
@@ -1,4 +0,0 @@
-from typing import Dict
-from datetime import datetime
-
-document_store: Dict[str, dict] = {}
---
+++ backend/app/core/document_store.py
@@ -0,0 +1,10 @@
+from typing import Dict
+from datetime import datetime
+
+document_store: Dict[str, dict] = {
+    "29088ebf-efa9-4652-85c8-2d9eb215f27d": {
+        "filename": "a-practical-guide-to-building-agents.pdf",
+        "num_chars": 35890,
+        "uploaded_at": "2025-06-23T19:20:09.174177+00:00",
+    }
+}
