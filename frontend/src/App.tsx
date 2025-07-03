09c2563977a4a1982e6e6da17ce68a5b55e009ef
Azim Baghadiya <azimsb120@gmail.com>
2025-07-03 17:18:15 -0400

    Clean React scaffold: remove unused assets, confirm Tailwind working

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex py-6 px-4">
+      <h1 className="text-xl font-bold">Document QnA</h1>
+    </div>
+  );
+}
+
+export default App;
